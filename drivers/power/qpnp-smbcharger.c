ecb5a721c8b0beff4ec65ccd4e3bd7fbb87327e0
dazzworld@163.com <dazzworld@163.com>
2017-01-13 15:29:09 +0800

    power:  rename missing ALLOW_DETECTION to DPF_DMF
    (cherry picked from commit 2f5836b6278efbdca20dac96a43b1abfa0c4e950)

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -5090,8 +5090,10 @@ static int determine_initial_status(struct smbchg_chip *chip)
 	#endif
 
 	if (chip->usb_present) {
-		pr_smb(PR_MISC, "setting usb psy allow detection 1\n");
+		pr_smb(PR_MISC, "setting usb psy dp=f dm=f\n");
 		power_supply_set_allow_detection(chip->usb_psy, 1);
+		power_supply_set_dp_dm(chip->usb_psy,
+				POWER_SUPPLY_DP_DM_DPF_DM);
 		handle_usb_insertion(chip);
 	} else {
 		handle_usb_removal(chip);
